1d51030a59f619722b6c1984475cd106fbb710d2
otmanoulcaid <ooulcaid.1337@gmail.com>
2025-02-06 06:22:32 -0600

    xml data is fetched

---
+++ public/js/script.js
@@ -0,0 +1,38 @@
+
+
+let add = () =>
+{
+
+}
+
+let remove = () =>
+{
+
+}
+
+let update = () =>
+{
+
+}
+	
+let getXhr = ()=>
+{
+
+}
+
+let serialize = () =>
+{
+
+}
+var details = (id) => 
+{
+	console.log(id);
+}
+
+document.addEventListener("DOMContentLoaded", ()=>
+{
+
+	let add = document.querySelectorAll();
+	let remove = document.querySelector();
+	let update = document.querySelector();
+});
